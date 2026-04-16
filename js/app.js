84641b1b9c7538dacce4122603779b794bf3c3be
kojo-duah <x.kojoduah@gmail.com>
2026-04-16 14:32:47 -0600

    updated app.js
    added the details for the main function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
+import { CONFIG } from "./config";
 import { renderFeedList, settingsButton } from "./settings";
 import { fetchNews, cycleNews } from "./news";
 import { detectLocation } from "./location";
 import { fetchWeather } from "./weather";
-function init() { ... }
+function init() {
+    //Settings
+    renderFeedList();
+    settingsButton();
+
+    //News
+    fetchNews();
+    setInterval(fetchNews, 5* 60 * 1000);
+    setInterval(cycleNews, CONFIG.cycle * 1000);
+
+    //Location
+    detectLocation();
+    setInterval(detectLocation, 60 * 60 * 1000);
+
+    //Weather
+    fetchWeather();
+    setInterval(fetchWeather, 15 * 60 * 1000);
+}
 document.addEventListener('DOMContentLoaded', init);
